0198c11232babe48241b379b76b6416cc0b8e7d1
Lucas Henrique Russo do Nascimento <lucnascimento98@gmail.com>
2022-05-02 23:03:40 -0300

    Events per month implementation

---
+++ src/Features/Dashboard/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Nudes.Retornator.Core;
+using SChallenge.Features.Dashboard.EventsPerMonth;
+using SChallenge.Models;
+
+namespace SChallenge.Features.Dashboard
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public DashboardController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet("EventsPerMonthOfAYear")]
+        public Task<ResultOf<List<EventsPerMonthDTO>>> EventsPerMonth([FromQuery]EventsPerMonthRequest eventsPerMonthRequest, CancellationToken cancellationToken)
+        {
+            return mediator.Send(eventsPerMonthRequest, cancellationToken);
+        }
+    }
+}
---
+++ src/Features/Dashboard/EventsPerMonth/EventsPerMonthHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nudes.Retornator.Core;
+using SChallenge.Domain;
+using SChallenge.Models;
+
+namespace SChallenge.Features.Dashboard.EventsPerMonth
+{
+    public class EventsPerMonthHandler : IRequestHandler<EventsPerMonthRequest, ResultOf<List<EventsPerMonthDTO>>>
+    {
+        private readonly EventManagerContext db;
+
+        public EventsPerMonthHandler(EventManagerContext db)
+        {
+            this.db = db;
+        }
+        public async Task<ResultOf<List<EventsPerMonthDTO>>> Handle(EventsPerMonthRequest request, CancellationToken cancellationToken)
+        {
+            return await db.Events
+                .Where(x => x.Date.Year == request.Year)
+                .GroupBy(x => x.Date.Month)
+                .Select(x => new EventsPerMonthDTO()
+                {
+                    Month = x.Key,
+                    Quantity = x.Count()
+                })
+                .OrderBy(x => x.Month)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
