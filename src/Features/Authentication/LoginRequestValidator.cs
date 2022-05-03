2ad850424eb66647ad8351be091a45fbaebda354
Lucas Henrique Russo do Nascimento <lucnascimento98@gmail.com>
2022-05-03 15:14:55 -0300

    Login email validation

---
+++ src/Features/Authentication/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace SChallenge.Features.Authentication
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
