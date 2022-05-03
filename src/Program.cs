2ff8cf2772e6cccf71ef80df58b5c05d9fd7ab1c
Lucas Henrique Russo do Nascimento <lucnascimento98@gmail.com>
2022-05-03 13:07:48 -0300

    Swagger documentation configuration

--- src/Program.cs
+++ src/Program.cs
@@ -11,13 +11,26 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Mapster;
 using System.Linq.Expressions;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.local.json", true, true);
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "SENAI Backend Challenge 01.01",
+        Description = "Esta API foi construida com o objetivo de testar o autor nas suas habilidades de desenvolvimento de software em backend, " +
+                    "para possivel aprovação em processo seletivo de contratação.",        
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 
 builder.Services.AddControllers().AddRetornator();
 
@@ -66,7 +79,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 app.UseAuthentication();
 
